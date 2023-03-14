9fcba22ed71fd65f69aea6f4f840b35a54753b5a
Liliia Baieva <555blv@gmail.com>
2023-03-14 13:29:43 +0100

    add constructor, getters and setters in class Food

--- src/HomeWork39/Food.java
+++ src/HomeWork39/Food.java
@@ -4,9 +4,36 @@ public class Food {
   //  Создать класс Food (еда). В классе должны быть:
 //  поле "название"
 //  поле "калорийность"
-//  конструктор, геттеры, сеттеры
-//  метод "употребить еду" с каким-нибудь выводом
 
+  private String name;
+  private int Calories;
 
+  //  конструктор
+  public Food(String name, int calories) {
+    this.name = name;
+    this.Calories = calories;
+  }
 
+  //  геттеры
+  public String getName() {
+    return name;
+  }
+
+  public int getCalories() {
+    return Calories;
+  }
+
+  //  сеттеры
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setCalories(int calories) {
+    Calories = calories;
+  }
+
+  //  метод "употребить еду" с каким-нибудь выводом
+  public void eatFood() {
+    System.out.printf("You eat %s (%d calories).", getName(), getCalories());
+  }
 }
