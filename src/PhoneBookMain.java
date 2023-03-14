4ba8dc672dcb588c081801abaa758a61266e59cb
Liliia Baieva <555blv@gmail.com>
2023-03-14 13:15:18 +0100

    add class PhoneBookMain

---
+++ src/PhoneBookMain.java
@@ -0,0 +1,26 @@
+import Phonebook.DigitalPerson;
+import Phonebook.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhoneBookMain {
+  public static void main(String[] args) {
+    List<Person> records = new ArrayList<>();
+    records.add(new Person("Кирилл", "+1234567"));
+    records.get(0).makeCall();
+    records.add(new Person("Мефодий", "+2345678"));
+
+    records.add(new DigitalPerson("Иннокентий", "kesha@gmail.com"));
+
+    printAllRecords(records);
+  }
+
+  public static void printAllRecords(List<Person> records) {
+    System.out.println();
+    System.out.println("Записи в телефонной книге:");
+    for (Person person : records) {
+      System.out.println(person.getPersonString());
+    }
+  }
+}
