25ab954ee18ad276dbd7097ebc44bf764f196191
Liliia Baieva <555blv@gmail.com>
2023-03-14 12:56:06 +0100

    add class Cat

---
+++ src/Pets/Cat.java
@@ -0,0 +1,11 @@
+package Pets;
+
+public class Cat extends Pet{
+  public Cat(String name, String dateOfBirth, double weight) {
+    super(name, dateOfBirth, weight);
+  }
+
+  public void makePurr() {
+    System.out.println(getName() + ": Мррр...");
+  }
+}
